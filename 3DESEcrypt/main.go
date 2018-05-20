33325bf43b6c30c6fbde026f34c32685e5f85677
WuEcho <emailforecho@163.com>
2018-05-20 15:35:49 +0800

    3DES加密

--- 3DESEcrypt/main.go
+++ 3DESEcrypt/main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"EncryptDemo/3DESEcrypt/3DES_CBC"
 	"encoding/base64"
+	"fmt"
 )
 
-func main()  {
+func main() {
 
 	var key []byte = []byte("123456789012345678901234")
 
-	var encryptCode = _DES_CBC.TripleDesEncrypt([]byte("helloworld"),key)
+	var encryptCode = _DES_CBC.TripleDesEncrypt([]byte("helloworld"), key)
 
 	fmt.Println(base64.StdEncoding.EncodeToString(encryptCode))
 
-	var decryptCode =  _DES_CBC.TripleDesDecrypt(encryptCode,key)
+	var decryptCode = _DES_CBC.TripleDesDecrypt(encryptCode, key)
 
 	fmt.Println(string(decryptCode))
 }
