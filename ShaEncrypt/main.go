120983293904bad52486ba5369a53d26ebef399f
WuEcho <emailforecho@163.com>
2018-05-20 15:41:26 +0800

    SHA256加密

--- ShaEncrypt/main.go
+++ ShaEncrypt/main.go
@@ -3,26 +3,24 @@ package main
 import (
 	"EncryptDemo/ShaEncrypt/sha256Encrypt"
 	"fmt"
-
 )
 
 func main() {
 
-  var encryptCode =	sha256Encrypt.Sha256EncryptTypeOne([]byte("hello world"))
+	var encryptCode = sha256Encrypt.Sha256EncryptTypeOne([]byte("hello world"))
 
-  fmt.Printf("%x",encryptCode)
+	fmt.Printf("%x", encryptCode)
 
-  fmt.Println()
+	fmt.Println()
 
-  var dencrypt = sha256Encrypt.Sha256EncryptTypeTwo([]byte("hello world"))
+	var dencrypt = sha256Encrypt.Sha256EncryptTypeTwo([]byte("hello world"))
 
-  fmt.Printf("%x",dencrypt)
+	fmt.Printf("%x", dencrypt)
 
-  fmt.Println()
-  fileEncrypted,err := sha256Encrypt.FileEncryptBySha256WithPath("/Users/wuxinyang/Desktop/MyGo/src/EncryptDemo/ShaEncrypt/test.txt")
+	fmt.Println()
+	fileEncrypted, err := sha256Encrypt.FileEncryptBySha256WithPath("/Users/wuxinyang/Desktop/MyGo/src/EncryptDemo/ShaEncrypt/test.txt")
 
-
-  if err == nil {
+	if err == nil {
 		fmt.Println(fileEncrypted)
 	}
 
